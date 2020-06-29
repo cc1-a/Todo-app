d259144775a879d2806c29f28e7604b4e68035d0
cc1-a <pgalagedara7@gmail.com>
2020-06-29 07:47:22 +0530

    Todo app

---
+++ todo/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/family/Vol_21/Amodh/Amodh Progaming/visual studio/django/todoapp/todo/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
